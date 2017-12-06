69f8b5fc61f55b51beb10065ce5e8c708f097a06
tac <514162920@qq.com>
2017-12-06 16:25:14 +0800

    import project

---
+++ drools/src/test/java/cn/tac/test/rule/drools/HelloWorldTest.java
@@ -0,0 +1,21 @@
+package cn.tac.test.rule.drools;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * @author tac
+ * @since 06/12/2017
+ */
+public class HelloWorldTest {
+    @Test
+    public void testHelloWorld() {
+        KieServices kieServices = KieServices.Factory.get();
+        KieContainer kieContainer = kieServices.newKieClasspathContainer();
+        KieSession kieSession = kieContainer.newKieSession("helloWorldSession");
+        kieSession.fireAllRules();
+        kieSession.dispose();
+    }
+}
---
+++ easy-rules/src/main/java/cn/tac/test/rule/easyrules/rule/FooRule.java
@@ -0,0 +1,22 @@
+package cn.tac.test.rule.easyrules.rule;
+
+import org.jeasy.rules.annotation.Action;
+import org.jeasy.rules.annotation.Condition;
+import org.jeasy.rules.annotation.Rule;
+
+/**
+ * @author tac
+ * @since 06/12/2017
+ */
+@Rule(name = "foo rule", description = "hello world")
+public class FooRule {
+    @Condition
+    public boolean when() {
+        return true;
+    }
+
+    @Action
+    public void then() throws Exception {
+        System.out.println("hello world");
+    }
+}
